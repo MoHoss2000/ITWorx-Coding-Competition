8ef833fcdd84170b274c5dc7f30e6af0e61c46e5
Alaa Amr Mansour <alaa.amr1@hotmail.com>
2021-08-28 16:53:04 +0200

    Registeration Token

--- backend/routes/general.js
+++ backend/routes/general.js
@@ -14,34 +14,66 @@ router.use(cookieParser())
 
 router.post("/register", async (req, res) => {
     // we take the input enetered by the user from the request
-    const { first_name, last_name, username, password , is_developer} = req.body;
+    const { first_name, last_name, username, password , is_developer, is_admin} = req.body;
 
-    
-    const employeeUser = await Employee.findOne({where :{ username: username}});
-    if(employeeUser) //username exists
-        res.status(400).json({error: "Username exists"});
-
-    const adminUser = await Admin.findOne({where :{ username: username}});
-    if(adminUser) //username exists
-        res.status(400).json({error: "Username exists"});
-
-    // we hash the password and then create an entry in the db with the hashed password
-    bcrypt.hash(password, 10).then((hash)=>{
-        Employee.create({
-            first_name: first_name,
-            last_name: last_name,
-            username: username,
-            password: hash,
-            is_developer: is_developer
-        }). then(()=> { // Another promise, to return a json response after the user has been added to the db
-            res.json("User successfully registered");
-        }).catch((err) => {
-            if (err) {
-              res.status(400).json({ error: err });
-            } 
-        });
-    });   
+    if(is_admin){ //register as admin
+        //check to see if an admin with the same username exists
+        const adminUser = await Admin.findOne({where :{ username: username}});
+        if(adminUser) //username exists
+            res.status(400).json({error: "Username exists"});
+        else{
+            bcrypt.hash(password, 10).then((hash)=>{
+            const admin = Admin.create({
+                          first_name: first_name,
+                          last_name: last_name,
+                          username: username,
+                          password: hash,
+                          is_developer: is_developer
+                }). then(()=> {
+                    const token = createToken(admin);
+                    //creating the cookie and saving it in the user's browser
+                    res.cookie("token", token, {
+                        maxAge: 60 * 60 * 24 * 30 * 1000, //30 days
+                        httpOnly: true,
+                    });
+                    res.json("User successfully registered");
+                }).catch((err) => {
+                    if (err) {
+                      res.status(400).json({ error: err });
+                    } 
+                });
+            }); 
+        }  
+    }
+    else{
+        const employeeUser = await Employee.findOne({where :{ username: username}});
+        if(employeeUser) //employee username exists
+            res.status(400).json({error: "Username exists"});
+            // we hash the password and then create an entry in the db with the hashed password
+            bcrypt.hash(password, 10).then((hash)=>{
+            const employee = Employee.create({
+                             first_name: first_name,
+                             last_name: last_name,
+                             username: username,
+                             password: hash,
+                             is_developer: is_developer
+                }). then(()=> { 
+                    const token = createToken(employee);
+                    //creating the cookie and saving it in the user's browser
+                    res.cookie("token", token, {
+                        maxAge: 60 * 60 * 24 * 30 * 1000, //30 days
+                        httpOnly: true,
+                    });
+                    res.json("User successfully registered");
+                }).catch((err) => {
+                    if (err) {
+                    res.status(400).json({ error: err });
+                    } 
+                });
+            });  
+        } 
 });
+
 
 router.post("/login", async (req, res) => {
     const { username, password } = req.body; 
@@ -73,7 +105,7 @@ router.post("/login", async (req, res) => {
                     if (!matched) {
                         res.status(400).json({ error: "Username or Password is incorrect" });
                     } else {
-                        const token = createToken(eadminmployee);
+                        const token = createToken(admin);
                         //creating the cookie and saving it in the user's browser
                         res.cookie("token", token, {
                             maxAge: 60 * 60 * 24 * 30 * 1000,
--- backend/utils/tokens.js
+++ backend/utils/tokens.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const createToken = (user)=> {
     //sign() creates a token containing all the data you need in the front-end
-    const token = jwt.sign(user.id, "" + process.env.JWT_SECRET);
+    const token = jwt.sign(user.id, "" + process.env.SECRET);
     return token;
 };
 
