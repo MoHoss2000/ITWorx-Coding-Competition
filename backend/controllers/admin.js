3e5b6152727522beb2d8491f7617c3149d2c1a7f
Mohamed Abdelhamid <mohamedabdelhamid3030@gmail.com>
2021-08-31 01:32:30 +0200

    .

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -2,9 +2,6 @@ const path = require('path')
 const excel = require('exceljs');
 const {Cycle} = require('../models')
 const proc = require('../db/procedures')
-const fs = require('fs');
-const https = require('https');
-
 
 exports.viewParticipants = async (req, res) => {
     const cycleID = req.params.cycleID
@@ -69,32 +66,3 @@ exports.viewProfile = async (req, res) => {
         return res.status(400).send()
     }
 }
-
-
-// exports.exportToExcelLeaderboard = async(req, res) => {
-//     const list = req.body.result
-//     const sort = req.body.sort
-//     let newList = []
-//     if(sort === 'employees' || sort === 'developers' || sort === 'non-developers')
-//     list.forEach((element) => {
-//         let newElement = element
-//         newElement.fullName = element.first_name + ' ' + element.last_name
-//         newList.push(newElement)
-//     })
-//     let workbook = new excel.Workbook()
-// 	let worksheet = workbook.addWorksheet(sort)
-//     worksheet.columns = [
-//         { header: 'Name', key: 'fullName', width: 30 },
-//         { header: 'Username', key: 'username', width: 30},
-//         { header: 'Developer', key: 'is_developer', width: 10, outlineLevel: 1}
-//     ];
-//     worksheet.addRows(newList);
-
-//     workbook.xlsx.writeFile("participants.xlsx")
-// 		.then(function() {
-// 			console.log("file saved!")
-//             const excelPath = __dirname.split('controllers')[0] + 'participants.xlsx'
-//             res.download(excelPath)
-//             res.status(200).send()
-// 	}).catch((e) => res.status(400).send(e));
-//}
