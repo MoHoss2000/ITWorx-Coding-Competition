bf7186c24ef17deed4a8003b3cee360b2e06b516
Mohamed Abdelhamid <mohamedabdelhamid3030@gmail.com>
2021-08-31 22:10:09 +0200

    check point

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const db = require("./models");
 //built-in middleware function in Express. It parses incoming requests with JSON 
 app.use(express.json());
 
-db.sequelize.sync().then(() => {
+db.sequelize.sync({force: true}).then(() => {
     app.listen(8080, () => {
       console.log("Server running on port 8080");
     });
