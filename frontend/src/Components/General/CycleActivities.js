f44f5410e7a8c67e3c419b74ca53a724735f50ba
Alaa Amr Mansour <alaa.amr1@hotmail.com>
2021-09-08 22:43:16 +0200

    Activities Component

--- frontend/src/Components/General/CycleActivities.js
+++ frontend/src/Components/General/CycleActivities.js
@@ -1,109 +1,52 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import 'antd/dist/antd.css';
-// import './index.css';
-// import { List, Avatar, Button, Skeleton } from 'antd';
+import React , {useState, useEffect} from 'react';
+import 'antd/dist/antd.css';
+import { List } from 'antd';
+import Spinner from './loadingSpinner'
 
-// import reqwest from 'reqwest';
+const ActivityList = () => {
+    const [loading, setLoading] = useState(true) 
+    const [data, setData] = useState([])
+    const [error, setError] = useState(null)
 
-// const count = 3;
-// const fakeDataUrl = `https://randomuser.me/api/?results=${count}&inc=name,gender,email,nat&noinfo`;
+    useEffect(() => {
+        axios(
+        {
+            method: 'get',
+            url: 'http://localhost:3001/admin/viewActivity',
+            headers: {}, 
+            data: { id, 
+                    CycleId : 1 }
+        }).then((res) =>{          
+                setData(res.data)
+                setLoading(false)               
+              })
+              .catch((e) => {
+                setError("Oops there seems to be a problem connecting to the network. Please try again later")
+                console.log(e)
+                
+                
+         })
+    
+    }, []);
 
-// function List () {
-//   state = {
-//     initLoading: true,
-//     loading: false,
-//     data: [],
-//     list: [],
-//   };
-
-//   componentDidMount() {
-//     this.getData(res => {
-//       this.setState({
-//         initLoading: false,
-//         data: res.results,
-//         list: res.results,
-//       });
-//     });
-//   }
-
-//   getData = callback => {
-//     reqwest({
-//       url: fakeDataUrl,
-//       type: 'json',
-//       method: 'get',
-//       contentType: 'application/json',
-//       success: res => {
-//         callback(res);
-//       },
-//     });
-//   };
-
-//   onLoadMore = () => {
-//     this.setState({
-//       loading: true,
-//       list: this.state.data.concat([...new Array(count)].map(() => ({ loading: true, name: {} }))),
-//     });
-//     this.getData(res => {
-//       const data = this.state.data.concat(res.results);
-//       this.setState(
-//         {
-//           data,
-//           list: data,
-//           loading: false,
-//         },
-//         () => {
-//           // Resetting window's offsetTop so as to display react-virtualized demo underfloor.
-//           // In real scene, you can using public method of react-virtualized:
-//           // https://stackoverflow.com/questions/46700726/how-to-use-public-method-updateposition-of-react-virtualized
-//           window.dispatchEvent(new Event('resize'));
-//         },
-//       );
-//     });
-//   };
-
-//   render() {
-//     const { initLoading, loading, list } = this.state;
-//     const loadMore =
-//       !initLoading && !loading ? (
-//         <div
-//           style={{
-//             textAlign: 'center',
-//             marginTop: 12,
-//             height: 32,
-//             lineHeight: '32px',
-//           }}
-//         >
-//           <Button onClick={this.onLoadMore}>loading more</Button>
-//         </div>
-//       ) : null;
-
-//     return (
-//       <List
-//         className="demo-loadmore-list"
-//         loading={initLoading}
-//         itemLayout="horizontal"
-//         loadMore={loadMore}
-//         dataSource={list}
-//         renderItem={item => (
-//           <List.Item
-//             actions={[<a key="list-loadmore-edit">edit</a>, <a key="list-loadmore-more">more</a>]}
-//           >
-//             <Skeleton avatar title={false} loading={item.loading} active>
-//               <List.Item.Meta
-//                 avatar={
-//                   <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-//                 }
-//                 title={<a href="https://ant.design">{item.name.last}</a>}
-//                 description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-//               />
-//               <div>content</div>
-//             </Skeleton>
-//           </List.Item>
-//         )}
-//       />
-//     );
-//   }
-// }
-
-// ReactDOM.render(<LoadMoreList />, document.getElementById('container'));
+    if(loading)
+        return <Spinner />
+        
+    return(
+    <List
+        itemLayout="horizontal"
+        dataSource={data}
+        renderItem={item => (
+        <List.Item
+            actions={[<a key="list-loadmore-more">more</a>]}
+        >
+            <List.Item.Meta
+              title={<a href="https://ant.design">{item.title}</a>}
+              description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+             />
+        </List.Item>
+        )}
+    />
+    )
+}
+  
