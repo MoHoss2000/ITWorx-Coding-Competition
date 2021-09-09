eb9e4e6379b6588d8bef452f075c5381ee2b1a0e
Ahmed Magdy <ahmedmagdy.200m@gmail.com>
2021-09-09 02:51:44 +0200

    Fix styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,52 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 import Activities from './Components/Admin/Activities';
 import React from 'react';
 import 'antd/dist/antd.css';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import {Layout} from 'antd';
 import AdminHome from './Components/AdminHome';
 import CreateActivity from './Components/Admin/CreateActivity';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch,} from 'react-router-dom'
 import HeaderAdmin from './Components/Navigation/HeaderAdmin';
-import SiderAdmin  from './Components/Navigation/SiderAdmin';
+import SiderAdmin from './Components/Navigation/SiderAdmin';
 
 
 function App() {
   return (
     <Router>
-  <Layout>
-  <Layout >
-  <Layout   style={{
-   height: '50px',
-   display: 'flex',
-   textAlign:"center",
-   width: "100%"
-   }}>
-   <HeaderAdmin/>
-    </Layout>
-  </Layout>
- <Layout style={{  display:'flex' , flexDirection :'row'}} >
-   
-  <Layout style={{width: '20%' }}>
-    <SiderAdmin/>
-  </Layout>
-  <Layout style={{width: '80%' }} >
-   
-      <Route exact path='/' component={AdminHome}/> 
-      <Route path='/activities' component={Activities}/> 
-      <Route path='/newActivity' component={CreateActivity}/>      
-  
-  </Layout>
-</Layout>    
- </Layout>
- </Router>
-     
-    
+      <Layout>
+        <Layout>
+          <Layout style={{
+            // height: '50px',
+            display: 'flex',
+            textAlign: "center",
+            width: "100%"
+          }}>
+            <HeaderAdmin/>
+          </Layout>
+        </Layout>
+        <Layout style={{display: 'flex', flexDirection: 'row'}}>
+
+          <Layout style={{flex: 0}}>
+            <SiderAdmin/>
+          </Layout>
+          <Layout style={{flex: 1, paddingLeft: 50, paddingTop: 50}}>
+            <Switch>
+              <Route exact path='/' component={AdminHome}/>
+              <Route path='/activities' component={Activities}/>
+              <Route path='/newActivity' component={CreateActivity}/>
+            </Switch>
+          </Layout>
+        </Layout>
+      </Layout>
+    </Router>
+
+
   );
 }
 
