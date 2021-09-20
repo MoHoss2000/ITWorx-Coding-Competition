711f25e0b61a44507ad9a4112d6882f1198fbcfd
Alaa Amr Mansour <alaa.amr1@hotmail.com>
2021-09-20 19:12:50 +0200

    Cycles Search Functionality

--- frontend/src/Components/Admin/CyclesList.js
+++ frontend/src/Components/Admin/CyclesList.js
@@ -1,25 +1,43 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import {UserContext} from '../../Context'
 import 'antd/dist/antd.css';
-import {Divider} from 'antd';
+import {Divider, Input} from 'antd';
 import {SyncOutlined, CarryOutOutlined} from '@ant-design/icons';
 import CycleCard from '../General/CycleCard'
 import '../components.css';
+const { Search } = Input;
 
 
-
-
-  
-    
 const CycleList = (props) =>{  
   const {cycleId} = useContext(UserContext)
+  const [searchTerm, setSearchTerm] = useState("")
+  const [filteredCycles, setFilteredCycles] = useState([]);
+
+  useEffect(() => {
+    setFilteredCycles(
+      props.cycles.filter((cycle) =>
+        cycle.start_date.includes(searchTerm) || cycle.end_date.includes(searchTerm) 
+      ) 
+    );
+  }, [searchTerm]);
+
   return( 
    <div>
       <h1 className= "title"> <b> Cycles History </b></h1>
       <Divider className="title-divider"/>
+      <div style={{display:'flex', flexDirection:'row-reverse'}}>
+      <Search
+        allowClear
+        enterButton="Search"
+        size="large"
+        style={{width:'30%', margin: '2%'}}
+        placeholder="Search Cycle By Date"
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      </div>
       <div style={{display:'flex', flexWrap:'wrap'}}> 
       {
-       props.cycles.map (({id, start_date,end_date}) => {
+       filteredCycles.map (({id, start_date,end_date}) => {
         if (id == cycleId){
           return (
             <CycleCard
